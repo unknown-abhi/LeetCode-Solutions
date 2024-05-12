77218d0275d76ccf529c5f3db400c7586871826d
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-05-13 00:48:20 +0530

    Time: 15 ms (26.08%) | Memory: 44.6 MB (37.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
         s = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         StringBuilder newString = new StringBuilder(s);
         newString.reverse();
