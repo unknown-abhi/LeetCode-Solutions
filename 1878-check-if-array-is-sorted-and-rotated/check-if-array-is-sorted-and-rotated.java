e6c7b63fc5d512e823984ca962d337cb16f9b086
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-05-20 03:47:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (71.99%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,45 +3,16 @@ class Solution {
         if (nums.length <= 2) {
             return true;
         }
-        boolean flag = false;
-
-        List<Integer> arr1 = new ArrayList<>();
-        List<Integer> arr2 = new ArrayList<>();
-        int secondCount = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (i == nums.length - 1) {
-                arr1.add(nums[i]);
-                return true;
-            } else if (nums[i] <= nums[i + 1]) {
-                arr1.add(nums[i]);
-            } else if (nums[i] > nums[i + 1]) {
-                arr1.add(nums[i]);
-                secondCount = i;
-                break;
+        int count = 0;
+        int size = nums.length;
+        for (int i = 0; i < size - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
             }
         }
-        for (int i = secondCount + 1; i < nums.length; i++) {
-            if (i == nums.length - 1) {
-                arr2.add(nums[i]);
-            } else if (nums[i] <= nums[i + 1]) {
-                arr2.add(nums[i]);
-            } else {
-                return false;
-            }
+        if (nums[0] < nums[size - 1]) {
+            count++;
         }
-
-        List<Integer> finalArr = new ArrayList<>();
-        finalArr.addAll(arr2);
-        finalArr.addAll(arr1);
-
-        for (int i = 0; i < finalArr.size() - 1; i++) {
-            if (finalArr.get(i) <= finalArr.get(i + 1)) {
-                flag = true;
-            } else {
-                flag = false;
-                break;
-            }
-        }
-        return flag;
+        return count <= 1;
     }
 }
