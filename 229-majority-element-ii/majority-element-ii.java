67fd9b88907c5ea0dcef0812da9830fb43db28cb
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-06-08 19:24:13 +0530

    Time: 18 ms (5.99%) | Memory: 47 MB (98.00%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
 
+        Arrays.sort(nums);
         HashMap<Integer, Integer> mp = new HashMap<>();
         int size = nums.length;
 
-        Set<Integer> temp = new HashSet<>();
+        ArrayList<Integer> ans = new ArrayList<>();
         int nBy3 = size / 3;
+        int last = Integer.MIN_VALUE;
 
         for (int i = 0; i < size; i++) {
             mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
 
-            if (mp.get(nums[i]) > nBy3) {
-                temp.add(nums[i]);
+            if (mp.get(nums[i]) > nBy3 && last != nums[i]) {
+                last = nums[i];
+                ans.add(last);
             }
         }
 
-        ArrayList<Integer> ans = new ArrayList<>(temp);
         return ans;
     }
 }
