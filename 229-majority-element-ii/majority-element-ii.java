5f5a8cdac457c9cb79cdce746f667fa50c5983d6
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-06-08 19:25:03 +0530

    Time: 17 ms (7.31%) | Memory: 47 MB (98.00%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -6,13 +6,12 @@ class Solution {
         int size = nums.length;
 
         ArrayList<Integer> ans = new ArrayList<>();
-        int nBy3 = size / 3;
         int last = Integer.MIN_VALUE;
 
         for (int i = 0; i < size; i++) {
             mp.put(nums[i], mp.getOrDefault(nums[i], 0) + 1);
 
-            if (mp.get(nums[i]) > nBy3 && last != nums[i]) {
+            if (mp.get(nums[i]) > (size / 3) && last != nums[i]) {
                 last = nums[i];
                 ans.add(last);
             }
