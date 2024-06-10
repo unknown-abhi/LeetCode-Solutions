605eefaf4d1b24da372f3a87b23644a23a4dfa8c
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-06-11 02:19:05 +0530

    Time: 18 ms (56.99%) | Memory: 43.9 MB (57.48%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -6,22 +6,34 @@ class Solution {
 
         int size = nums.length;
         for (int i = 0; i < size; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < size; j++) {
-                Set<Integer> tmpSt = new HashSet<>();
-                for (int k = j + 1; k < size; k++) {
-                    int forth = target - (nums[i] + nums[j] + nums[k]);
+                if (j > (i + 1) && nums[j] == nums[j - 1])
+                    continue;
 
-                    if (tmpSt.contains(forth)
-                            && (long) nums[i] + (long) nums[j] + (long) nums[k] + (long) forth == target) {
+                int k = j + 1;
+                int l = size - 1;
+                while (k < l) {
+                    long sum = (long) nums[i] + (long) nums[j] + (long) nums[k] + (long) nums[l];
+                    if (sum == target) {
                         List<Integer> tmp = new ArrayList<>();
                         tmp.add(nums[i]);
                         tmp.add(nums[j]);
                         tmp.add(nums[k]);
-                        tmp.add(forth);
+                        tmp.add(nums[l]);
                         st.add(tmp);
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1])
+                            k++;
+                        while (k < l && nums[l] == nums[l + 1])
+                            l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
                     }
-                    tmpSt.add(nums[k]);
-
                 }
 
             }
