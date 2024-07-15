fe844cf95bf0cb4309b911c1a1652359af859e1d
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-07-16 02:12:03 +0530

    Time: 16 ms (6.83%) | Memory: 47.5 MB (11.29%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean minCapacity(int[] weights, int days, int minWeight) {
+
+        int noOfDays = 0;
+        int count = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            count += weights[i];
+
+            if (count == minWeight) {
+                noOfDays++;
+                count = 0;
+            } else if (count > minWeight) {
+                noOfDays++;
+                count = weights[i];
+            }
+        }
+
+        if (count < minWeight && count != 0) {
+            noOfDays++;
+        }
+
+        return noOfDays <= days;
+    }
+
+    public int shipWithinDays(int[] weights, int days) {
+
+        int low = Arrays.stream(weights).max().getAsInt();
+        int high = Arrays.stream(weights).sum();
+
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (minCapacity(weights, days, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                // ans = mid;
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
