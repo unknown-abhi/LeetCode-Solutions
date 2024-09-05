4cb1b730e6552a71f84eb4cfdcb4e83c36d9e0de
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-09-05 23:32:43 +0530

    Time: 15 ms (51.26%) | Memory: 45.7 MB (45.19%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -6,12 +6,13 @@ class Solution {
         }
 
         List<Character> characters = new ArrayList<>(frequencyMap.keySet());
-        Collections.sort(characters, new Comparator<Character>() {
-            @Override
-            public int compare(Character c1, Character c2) {
-                return frequencyMap.get(c2) - frequencyMap.get(c1);
-            }
-        });
+        // Collections.sort(characters, new Comparator<Character>() {
+        // @Override
+        // public int compare(Character c1, Character c2) {
+        // return frequencyMap.get(c2) - frequencyMap.get(c1);
+        // }
+        // });
+        characters.sort((c1, c2) -> frequencyMap.get(c2) - frequencyMap.get(c1));
 
         StringBuilder result = new StringBuilder();
         for (char c : characters) {
@@ -21,33 +22,33 @@ class Solution {
         }
         return result.toString();
     }
-// public String frequencySort(String s) {
-// Map<Character, Integer> mp = new TreeMap<>();
-// for (int i = 0; i < s.length(); i++) {
-// mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i), 0) + 1);
-// }
+    // public String frequencySort(String s) {
+    // Map<Character, Integer> mp = new TreeMap<>();
+    // for (int i = 0; i < s.length(); i++) {
+    // mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i), 0) + 1);
+    // }
 
-// StringBuilder sb = new StringBuilder();
+    // StringBuilder sb = new StringBuilder();
 
-// while (!mp.isEmpty()) {
-// int max = -1;
-// char tmpChar = '\0';
-// for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
-// int tmpMax = -1;
-// tmpMax = Math.max(tmpMax, entry.getValue());
-// if (tmpMax >= max) {
-// max = tmpMax;
-// tmpChar = entry.getKey();
-// tmpMax = -1;
-// }
-// }
+    // while (!mp.isEmpty()) {
+    // int max = -1;
+    // char tmpChar = '\0';
+    // for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
+    // int tmpMax = -1;
+    // tmpMax = Math.max(tmpMax, entry.getValue());
+    // if (tmpMax >= max) {
+    // max = tmpMax;
+    // tmpChar = entry.getKey();
+    // tmpMax = -1;
+    // }
+    // }
 
-// for (int i = 0; i < max; i++) {
-// sb.append(tmpChar);
-// }
-// mp.remove(tmpChar);
-// }
+    // for (int i = 0; i < max; i++) {
+    // sb.append(tmpChar);
+    // }
+    // mp.remove(tmpChar);
+    // }
 
-// return sb.toString();
-// }
+    // return sb.toString();
+    // }
 }
