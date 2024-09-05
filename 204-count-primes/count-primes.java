0ed06e6dd80738466d07a542286f06eeb7667911
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-09-05 07:31:11 +0530

    Time: 117 ms (25.00%) | Memory: 63.9 MB (22.08%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,20 +1,23 @@
 class Solution {
     public int countPrimes(int n) {
+        if (n <= 2) {
+            return 0;
+        }
 
         int[] ans = new int[n + 1];
         Arrays.fill(ans, 1);
 
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (ans[i] == 1) {
-                for(int j = i * i; j <= n; j += i){
+                for (int j = i * i; j <= n; j += i) {
                     ans[j] = 0;
                 }
             }
         }
 
         int count = 0;
-        for(int i = n - 1; i >= 2; i--){
-            if(ans[i] == 1){
+        for (int i = n - 1; i >= 2; i--) {
+            if (ans[i] == 1) {
                 count++;
             }
         }
