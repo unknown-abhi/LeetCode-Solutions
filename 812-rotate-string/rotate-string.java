d61cf547ef207dd7a6e0fe31ca35b50b63e9d3db
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-09-05 21:10:31 +0530

    Time: 1 ms (22.52%) | Memory: 41.3 MB (69.23%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,32 +1,22 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // if (s.length() != goal.length()) {
-        //     return false;
-        // }
-
-        Queue<Character> q1 = new LinkedList<>();
-        Queue<Character> q2 = new LinkedList<>();
-
-        for (char c : s.toCharArray()) {
-            q1.add(c);
+        if (s.length() != goal.length()) {
+            return false;
         }
-        for (char c : goal.toCharArray()) {
-            q2.add(c);
-        }
+        StringBuffer sb = new StringBuffer(s);
 
-        int len = q1.size();
+        int len = s.length();
 
         while (len > 0) {
-            char tmp = q1.peek();
-            q1.poll();
-            q1.add(tmp);
+            char tmp = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(tmp);
 
-            if (q1.equals(q2)) {
+            if (sb.toString().equals(goal)) {
                 return true;
             }
             len--;
         }
-
         return false;
     }
 }
