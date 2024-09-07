5e4029dad94e090f93ef740288b38f351b23e7ec
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-09-08 05:20:08 +0530

    Time: 5 ms (58.55%) | Memory: 44.4 MB (93.61%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -11,21 +11,34 @@ class Solution {
 
         int n = s.length();
         int result = 0;
+
         for (int i = 0; i < n; i++) {
             char current = s.charAt(i);
             int currentValue = romanValues.get(current);
-            if (i + 1 < n && romanValues.containsKey(s.charAt(i + 1))) {
-                int nextValue = romanValues.get(s.charAt(i + 1));
-                if (currentValue >= nextValue) {
-                    result += currentValue;
-                } else {
-                    result += nextValue - currentValue;
-                    i++;
-                }
+
+            if (i + 1 < n && currentValue < romanValues.get(s.charAt(i + 1))) {
+                result -= currentValue;
             } else {
                 result += currentValue;
             }
         }
+
+        // for (int i = 0; i < n; i++) {
+        // char current = s.charAt(i);
+        // int currentValue = romanValues.get(current);
+        // if (i + 1 < n && romanValues.containsKey(s.charAt(i + 1))) {
+        // int nextValue = romanValues.get(s.charAt(i + 1));
+        // if (currentValue >= nextValue) {
+        // result += currentValue;
+        // } else {
+        // result += nextValue - currentValue;
+        // i++;
+        // }
+        // } else {
+        // result += currentValue;
+        // }
+        // }
+
         return result;
     }
 }
