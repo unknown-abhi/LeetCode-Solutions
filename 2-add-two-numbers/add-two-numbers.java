3246661814795207827325ca16d74b7ec70d2ef0
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-10-16 00:09:09 +0530

    Time: 1 ms (100.00%) | Memory: 44.2 MB (70.70%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -16,7 +16,7 @@ class Solution {
         int carry = 0; // To handle carries during addition
 
         // Traverse both lists
-        while (l1 != null || l2 != null) {
+        while (l1 != null || l2 != null || carry != 0) {
             int x = (l1 != null) ? l1.val : 0; // Get value from l1 or 0 if l1 is null
             int y = (l2 != null) ? l2.val : 0; // Get value from l2 or 0 if l2 is null
 
@@ -33,9 +33,9 @@ class Solution {
         }
 
         // If carry is left after the last addition, create a new node for it
-        if (carry > 0) {
-            current.next = new ListNode(carry);
-        }
+        // if (carry > 0) {
+        //     current.next = new ListNode(carry);
+        // }
 
         return dummyHead.next; // Return the actual head (next of dummy node)
     }
