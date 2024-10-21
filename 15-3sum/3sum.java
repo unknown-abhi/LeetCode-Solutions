ad2b0c810fd2d5821f7d1ea85b4ec38fb1baadb2
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-10-21 13:54:37 +0530

    Time: 30 ms (68.05%) | Memory: 51.5 MB (49.34%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
 
         for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]){
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
@@ -13,18 +13,23 @@ class Solution {
             int k = nums.length - 1;
 
             while (j < k) {
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    // List<Integer> list = Arrays.asList(nums[i],nums[j],nums[k]);
                     // list.add(nums[i]);
                     // list.add(nums[j]);
                     // list.add(nums[k]);
-                    st.add(list);
+                    // st.add(list);
+                    st.add(Arrays.asList(nums[i], nums[j], nums[k]));
                     j++;
                     k--;
-                    while (j < k && nums[j] == nums[j-1]) j++;
-                    while (j < k && nums[k] == nums[k+1]) k--;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
 
-                } else if (nums[i] + nums[j] + nums[k] < 0) {
+                } else if (sum < 0) {
                     j++;
                 } else {
                     k--;
