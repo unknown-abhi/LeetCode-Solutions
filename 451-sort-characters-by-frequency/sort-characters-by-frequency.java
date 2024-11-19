610b1362d4205ce4d4b22b329ea293ec5db7e36c
Abhishek Kumar <49164677+unknown-abhi@users.noreply.github.com>
2024-11-20 01:06:43 +0530

    Time: 17 ms (35.32%) | Memory: 45.7 MB (34.27%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,54 +1,25 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> frequencyMap = new HashMap<>();
+        Map<Character, Integer> charCount = new HashMap<>();
+
         for (char c : s.toCharArray()) {
-            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
         }
 
-        List<Character> characters = new ArrayList<>(frequencyMap.keySet());
-        // Collections.sort(characters, new Comparator<Character>() {
-        // @Override
-        // public int compare(Character c1, Character c2) {
-        // return frequencyMap.get(c2) - frequencyMap.get(c1);
-        // }
-        // });
-        characters.sort((c1, c2) -> frequencyMap.get(c2) - frequencyMap.get(c1));
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(
+                (c1, c2) -> c2.getValue() - c1.getValue());
+
+        maxHeap.addAll(charCount.entrySet());
 
         StringBuilder result = new StringBuilder();
-        for (char c : characters) {
-            for (int i = 0; i < frequencyMap.get(c); i++) {
-                result.append(c);
+
+        while (!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> entry = maxHeap.poll();
+            for (int i = 0; i < entry.getValue(); i++) {
+                result.append(entry.getKey());
             }
         }
         return result.toString();
     }
-    // public String frequencySort(String s) {
-    // Map<Character, Integer> mp = new TreeMap<>();
-    // for (int i = 0; i < s.length(); i++) {
-    // mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i), 0) + 1);
-    // }
 
-    // StringBuilder sb = new StringBuilder();
-
-    // while (!mp.isEmpty()) {
-    // int max = -1;
-    // char tmpChar = '\0';
-    // for (Map.Entry<Character, Integer> entry : mp.entrySet()) {
-    // int tmpMax = -1;
-    // tmpMax = Math.max(tmpMax, entry.getValue());
-    // if (tmpMax >= max) {
-    // max = tmpMax;
-    // tmpChar = entry.getKey();
-    // tmpMax = -1;
-    // }
-    // }
-
-    // for (int i = 0; i < max; i++) {
-    // sb.append(tmpChar);
-    // }
-    // mp.remove(tmpChar);
-    // }
-
-    // return sb.toString();
-    // }
 }
